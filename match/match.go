6c6c8526a4d6b0cfebfdb56f0cae09554856791c
Santiago Cardozo Q <santiago.cardozo@ceiba.com.co>
2021-09-10 10:24:07 -0500

    Implementacion leer archivos con libreria os

--- match/match.go
+++ match/match.go
@@ -1,3 +1,25 @@
 package main
 
-func main() {}
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func leerArchivo() {
+	archivo, err := os.Open("./solicitudes_compra.cvs")
+	if err != nil {
+		fmt.Println("Error" + err.Error())
+	} else {
+		scanner := bufio.NewScanner(archivo)
+		for scanner.Scan() {
+			registro := scanner.Text()
+			fmt.Printf("linea > " + registro + "\n")
+		}
+	}
+	archivo.Close()
+}
+
+func main() {
+	leerArchivo()
+}

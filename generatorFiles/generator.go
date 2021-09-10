c66d6c496e0c2ae55d769082d3e0b99560d8aaa2
luismiguel010 <luismiguel010@gmail.com>
2021-09-10 14:23:41 -0500

    primer algoritmo con la solución

--- generatorFiles/generator.go
+++ generatorFiles/generator.go
@@ -7,8 +7,11 @@ import (
 	"time"
 )
 
+var nameFileBuy string = "./solicitudes_compra.cvs"
+var nameFileSale string = "./solicitudes_venta.cvs"
+
 func generateFile(nameFile string) {
-	file, err := os.Create("./" + nameFile + ".cvs")
+	file, err := os.Create(nameFile)
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -18,7 +21,7 @@ func generateFile(nameFile string) {
 	max := 10000
 	min := 5000
 
-	for i := 1; i <= 10; i++ {
+	for i := 1; i <= 5000; i++ {
 		rand.Seed(time.Now().UnixNano())
 		cantidad := rand.Intn(max-min) + min
 		fmt.Fprintf(file, "o%d,energiaBasica,%d,%s\n", i, cantidad, time.Now().UTC())
@@ -26,6 +29,6 @@ func generateFile(nameFile string) {
 }
 
 func main() {
-	generateFile("solicitudes_compra")
-	generateFile("solicitudes_venta")
+	generateFile(nameFileBuy)
+	generateFile(nameFileSale)
 }
--- match/match.go
+++ match/match.go
@@ -1,31 +1,15 @@
 package main
 
 import (
-	"bufio"
 	"encoding/csv"
 	"fmt"
 	"os"
+	"strconv"
+	"time"
 )
 
-func leerArchivo() {
-	archivo, err := os.Open("./solicitudes_compra.cvs")
-	if err != nil {
-		fmt.Println("Error" + err.Error())
-	} else {
-		scanner := bufio.NewScanner(archivo)
-		for scanner.Scan() {
-			registro := scanner.Text()
-			fmt.Printf("linea > " + registro + "\n")
-		}
-	}
-	archivo.Close()
-}
-
-func main() {
-	leerArchivo()
-	records := readCsvFile("./solicitudes_compra.cvs")
-	fmt.Println(records[0][2])
-}
+var nameFileBuy string = "./solicitudes_compra.cvs"
+var nameFileSale string = "./solicitudes_venta.cvs"
 
 func readCsvFile(filePath string) [][]string {
 	f, err := os.Open(filePath)
@@ -40,4 +24,73 @@ func readCsvFile(filePath string) [][]string {
 		fmt.Println("Unable to parse file as CSV for "+filePath, err)
 	}
 	return records
+}
+
+func sumColumn(valuesBuy [][]string) int {
+	colSum := 0
+	for i, rows := range valuesBuy {
+		for j := range rows {
+			valMatrix, _ := strconv.Atoi(valuesBuy[j][i])
+			colSum = colSum + valMatrix
+		}
+	}
+	return colSum
+}
+
+func match(valuesBuy [][]string, valuesSale [][]string) {
+	var i int = 0
+	var j int = 0
+
+	for {
+
+		a, _ := strconv.Atoi(valuesBuy[i][2])
+		b, _ := strconv.Atoi(valuesSale[j][2])
+
+		if b > a {
+			b = b - a
+			a = 0
+			valuesBuy[i][2] = strconv.Itoa(a)
+			valuesSale[j][2] = strconv.Itoa(b)
+			i++
+		} else if b < a {
+			a = a - b
+			b = 0
+			valuesBuy[i][2] = strconv.Itoa(a)
+			valuesSale[j][2] = strconv.Itoa(b)
+			j++
+		} else {
+			valuesBuy[i][2] = strconv.Itoa(0)
+			valuesSale[j][2] = strconv.Itoa(0)
+			i++
+			j++
+		}
+		if i == len(valuesBuy) || j == len(valuesBuy) {
+			break
+		}
+	}
+
+	generatorResult("solicitudes_compra_result.cvs", valuesBuy)
+	generatorResult("solicitudes_venta_result.cvs", valuesSale)
+
+}
+
+func generatorResult(nameFile string, values [][]string) {
+	file, err := os.Create(nameFile)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+
+	for _, value := range values {
+		fmt.Fprintf(file, "%s\n", value)
+	}
+}
+
+func main() {
+	start := time.Now()
+	recordsBuy := readCsvFile(nameFileBuy)
+	recordsSale := readCsvFile(nameFileSale)
+	match(recordsBuy, recordsSale)
+	fmt.Println(time.Since(start))
 }

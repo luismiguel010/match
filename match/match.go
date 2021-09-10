7169151e57daeba68bf07aa5820deae2f9014bd6
luismiguel010 <luismiguel010@gmail.com>
2021-09-10 14:45:57 -0500

    Solución2. Implementación de recursividad en función match

--- match/match.go
+++ match/match.go
@@ -10,6 +10,8 @@ import (
 
 var nameFileBuy string = "./solicitudes_compra.cvs"
 var nameFileSale string = "./solicitudes_venta.cvs"
+var i int = 0
+var j int = 0
 
 func readCsvFile(filePath string) [][]string {
 	f, err := os.Open(filePath)
@@ -26,52 +28,34 @@ func readCsvFile(filePath string) [][]string {
 	return records
 }
 
-func sumColumn(valuesBuy [][]string) int {
-	colSum := 0
-	for i, rows := range valuesBuy {
-		for j := range rows {
-			valMatrix, _ := strconv.Atoi(valuesBuy[j][i])
-			colSum = colSum + valMatrix
-		}
-	}
-	return colSum
-}
+func match(valuesBuy [][]string, valuesSale [][]string) ([][]string, [][]string) {
 
-func match(valuesBuy [][]string, valuesSale [][]string) {
-	var i int = 0
-	var j int = 0
-
-	for {
-
-		a, _ := strconv.Atoi(valuesBuy[i][2])
-		b, _ := strconv.Atoi(valuesSale[j][2])
-
-		if b > a {
-			b = b - a
-			a = 0
-			valuesBuy[i][2] = strconv.Itoa(a)
-			valuesSale[j][2] = strconv.Itoa(b)
-			i++
-		} else if b < a {
-			a = a - b
-			b = 0
-			valuesBuy[i][2] = strconv.Itoa(a)
-			valuesSale[j][2] = strconv.Itoa(b)
-			j++
-		} else {
-			valuesBuy[i][2] = strconv.Itoa(0)
-			valuesSale[j][2] = strconv.Itoa(0)
-			i++
-			j++
-		}
-		if i == len(valuesBuy) || j == len(valuesBuy) {
-			break
-		}
+	if i == len(valuesBuy) || j == len(valuesBuy) {
+		return valuesBuy, valuesSale
 	}
 
-	generatorResult("solicitudes_compra_result.cvs", valuesBuy)
-	generatorResult("solicitudes_venta_result.cvs", valuesSale)
+	a, _ := strconv.Atoi(valuesBuy[i][2])
+	b, _ := strconv.Atoi(valuesSale[j][2])
 
+	if b > a {
+		b = b - a
+		a = 0
+		valuesBuy[i][2] = strconv.Itoa(a)
+		valuesSale[j][2] = strconv.Itoa(b)
+		i++
+	} else if b < a {
+		a = a - b
+		b = 0
+		valuesBuy[i][2] = strconv.Itoa(a)
+		valuesSale[j][2] = strconv.Itoa(b)
+		j++
+	} else {
+		valuesBuy[i][2] = strconv.Itoa(0)
+		valuesSale[j][2] = strconv.Itoa(0)
+		i++
+		j++
+	}
+	return match(valuesBuy, valuesSale)
 }
 
 func generatorResult(nameFile string, values [][]string) {
@@ -91,6 +75,8 @@ func main() {
 	start := time.Now()
 	recordsBuy := readCsvFile(nameFileBuy)
 	recordsSale := readCsvFile(nameFileSale)
-	match(recordsBuy, recordsSale)
+	valuesBuy, valuesSale := match(recordsBuy, recordsSale)
+	generatorResult("solicitudes_compra_result.cvs", valuesBuy)
+	generatorResult("solicitudes_venta_result.cvs", valuesSale)
 	fmt.Println(time.Since(start))
 }

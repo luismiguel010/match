e7119efc3d06ae938371ca8b472ba456e82cd303
Santiago Cardozo Q <santiago.cardozo@ceiba.com.co>
2021-09-13 09:58:51 -0500

    Leer cantidad por pantalla

--- generatorFiles/generator.go
+++ generatorFiles/generator.go
@@ -11,18 +11,19 @@ var nameFileBuy string = "./solicitudes_compra.cvs"
 var nameFileSale string = "./solicitudes_venta.cvs"
 
 func generateFile(nameFile string) {
+	var amount int
+	fmt.Println("Ingrese la cantidad a generar")
+	fmt.Scanln(&amount)
 	file, err := os.Create(nameFile)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	defer file.Close()
-
 	max := 10000
 	min := 5000
 	var nameIdenficator string
-
-	for i := 1; i <= 10; i++ {
+	for i := 1; i <= amount; i++ {
 		rand.Seed(time.Now().UnixNano())
 		cantidad := rand.Intn(max-min) + min
 		if nameFile == nameFileBuy {

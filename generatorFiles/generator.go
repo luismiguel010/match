a61a5043ba6d51f9f6348bdd50acb12267ffbccc
luismiguel010 <luismiguel010@gmail.com>
2021-09-15 10:23:20 -0500

    Se generan archivos con precio y tolerancia y se unifica para un mismo valor de cantidad de solicitudes

--- generatorFiles/generator.go
+++ generatorFiles/generator.go
@@ -11,44 +11,56 @@ import (
 var nameFileBuy string = "./solicitudes_compra.cvs"
 var nameFileSale string = "./solicitudes_venta.cvs"
 
-func generateFile(nameFile string, wg *sync.WaitGroup, lock *sync.Mutex) {
-	var amount int
+func generateFile(nameFile string, amount int, wg *sync.WaitGroup, lock *sync.Mutex) {
+	var nameIdenficator string
+	var maxValues int = 10000
+	var minValues int = 5000
+	var maxCost int = 20
+	var minCost int = 10
+	var maxTol int = 3
+	var minTol int = 0
+	var value int
+	var cost int
+	var tol int
+
 	defer wg.Done()
-	lock.Lock()
-	fmt.Println("Ingrese la cantidad a generar para" + nameFile)
-	fmt.Scanln(&amount)
-	lock.Unlock()
-	start := time.Now()
 	file, err := os.Create(nameFile)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	defer file.Close()
-	max := 10000
-	min := 5000
-	var nameIdenficator string
 	for i := 1; i <= amount; i++ {
 		rand.Seed(time.Now().UnixNano())
-		cantidad := rand.Intn(max-min) + min
+		value = rand.Intn(maxValues-minValues) + minValues
+		cost = rand.Intn(maxCost-minCost) + minCost
+		tol = rand.Intn(maxTol-minTol) + minTol
 		if nameFile == nameFileBuy {
 			nameIdenficator = "C"
+			fmt.Fprintf(file, "%s%d,energiaBasica,%d,%d,%d,%s\n", nameIdenficator, i, value, cost, tol, time.Now().UTC())
 		} else {
 			nameIdenficator = "V"
+			fmt.Fprintf(file, "%s%d,energiaBasica,%d,%d,%s\n", nameIdenficator, i, value, cost, time.Now().UTC())
 		}
-		fmt.Fprintf(file, "%s%d,energiaBasica,%d,%s\n", nameIdenficator, i, cantidad, time.Now().UTC())
 	}
-	fmt.Println(time.Since(start))
 }
 
 func main() {
 
 	var lock sync.Mutex
 	var wg sync.WaitGroup
-	wg.Add(1)
-	go generateFile(nameFileBuy, &wg, &lock)
-	wg.Add(1)
-	go generateFile(nameFileSale, &wg, &lock)
+
+	var amount int
+	fmt.Println("Ingrese la cantidad a generar de los dos archivos")
+	fmt.Scanln(&amount)
+
+	if amount != 0 {
+		wg.Add(1)
+		go generateFile(nameFileBuy, amount, &wg, &lock)
+		wg.Add(1)
+		go generateFile(nameFileSale, amount, &wg, &lock)
+	}
+
 	wg.Wait()
 
 }
